b8574be4d78db54044a2e054bea387de0a9b63fe
Jay <46762446+starfoxdot64@users.noreply.github.com>
2022-11-04 08:34:15 -0700

    Experimental file shuffling between CVG2 and previous version of Infinity Kernel

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,5 +5,5 @@ export PLATFORM_VERSION=11
 export ANDROID_MAJOR_VERSION=r
 export ARCH=arm64
 
-make ARCH=arm64 exynos7885-gta3xlwifi_defconfig
+make ARCH=arm64 exynos7885-gta3xlwifi_defconfig.gsi
 make ARCH=arm64 -j64
